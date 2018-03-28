bd849e8c1a24bb984085ace05ad3b77bf5f74311
sudharsan gandhi <sudharsangandhi@gmail.com>
2018-03-28 12:11:55 +0530

    bulkdocs updation on single call

--- common-routes.js
+++ common-routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const common = require("./common");
+const http = require("axios");
 const	dbConfig		= 	require('./database');
 const 	db 				= 	dbConfig.db;
 const 	dbName			= 	dbConfig.dbName;
@@ -16,42 +17,11 @@ router.post('/token',function(req,res){
 
 router.post('/sync',function(req,res){
     console.log('request:',req.body);
-    db.get(dbName, req.body._id).then(({data, headers, status}) => {
-            console.log( "pouch "+ JSON.stringify(req.body));
-            req.body._rev = data._rev
-
-            if(data.timestamp < req.body.timestamp){
-                db.update(dbName,req.body)
-                .then(({data, headers, status}) => {
-                    res.json({success_msg: 'successfully Update up'});
-                })
-                .catch(err => {
-                    console.log("error:", err);
-                    res.status(400).send({ error_msg: err+" Update error" });
-                });
-        }
-        else if(data.timestamp > req.body.timestamp){
-            res.json(data)
-        }
-        else{
-            res.json({ms: "Already up to date"})
-        }
-
-
-        console.log("couch "+data._rev+" pouch "+ req.body._rev);
-        // res.json(data);
-    }, err => {
-        // either request error occured
-        // ...or err.code=EDOCMISSING if document is missing
-        // ...or err.code=EUNKNOWN if statusCode is unexpected
-        db.insert(dbName,req.body)
-                .then(({data, headers, status}) => {
-                    res.json({success_msg: 'successfully Inserted up'});
-                })
-                .catch(err => {
-                    console.log("error:", err);
-                    res.status(400).send({ error_msg: err+" New Req error" });
-                });
-    });
+    http.post("localhost:5984/fci/_bulk_docs",req.body)
+    .then((data) => res.json('sucess bulkdocs:', data)
+    ).catch((err)=> {
+        res.json("error bulkdocs:", err);
+    })
+   
 })
 module.exports = router;
